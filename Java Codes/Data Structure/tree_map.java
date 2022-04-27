12596304f36d7a9074b5dafc61821a0a1f7cdb9c
Aditya Dadheech <90897379+F1oppy@users.noreply.github.com>
2022-04-27 14:30:54 +0530

    Add files via upload

---
+++ Java Codes/Data Structure/tree_map.java
@@ -0,0 +1,24 @@
+package Data_Structure;
+
+import java.util.*;
+
+public class tree_map {
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	public static void main(String[] args) {
+		
+		TreeMap m1 = new TreeMap();
+		m1.put(1, "Amit");
+		m1.put(2, "Rahul");
+		m1.put(3, "Jai");
+		m1.put(4, "Amit");
+		
+		Set s1 = m1.entrySet();
+		
+		Iterator i = s1.iterator();
+		
+		while(i.hasNext()){
+			Map.Entry entry = (Map.Entry)i.next();
+			System.out.println(entry.getKey()+" "+entry.getValue());
+		}
+	}
+}
